eb7cd4f18792c285d5a608504c31d0b968b92fa1
user19971212 <rooster100@naver.com>
2022-04-11 19:15:59 +0900

    makeSecond

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const engine = require('ejs-mate');
 
+
+app.engine('ejs', engine);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+
 
 app.get('/', (req, res)=>{
     res.render('music/home');
